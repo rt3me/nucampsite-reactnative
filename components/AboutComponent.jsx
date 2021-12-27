31047699d3d49e1f95e8cc727753c14e77d97004
Robert Turner <robturneris@gmail.com>
2021-12-26 22:40:48 -0500

    Updated About component to use Loading component

--- components/AboutComponent.jsx
+++ components/AboutComponent.jsx
@@ -3,6 +3,7 @@ import { ScrollView, Text, FlatList } from "react-native";
 import { Card, ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "./LoadingComponent";
 
 const mapStateToProps = (state) => {
   return {
@@ -31,8 +32,26 @@ class About extends Component {
       return <ListItem title={item.name} subtitle={item.description} leftAvatar={{ source: { uri: baseUrl + item.image } }} />;
     };
 
-    console.log("Partneers", this.props.partners);
-
+    if (this.props.partners.isLoading) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title="Community Partners">
+            <Loading />
+          </Card>
+        </ScrollView>
+      );
+    }
+    if (this.props.partners.errMess) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title="Community Partners">
+            <Text>{this.props.partners.errMess}</Text>
+          </Card>
+        </ScrollView>
+      );
+    }
     return (
       <ScrollView>
         <Mission />

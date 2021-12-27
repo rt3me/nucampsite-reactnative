ce91a389407629279ae06d4db3d03cbf271ecaad
Robert Turner <robturneris@gmail.com>
2021-12-26 23:03:02 -0500

    Updated Directory component to use Loading component

--- components/DirectoryComponent.jsx
+++ components/DirectoryComponent.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { FlatList } from "react-native";
+import { View, FlatList, Text } from "react-native";
 import { ListItem } from "react-native-elements";
 import { CAMPSITES } from "../shared/campsites";
 import { Tile } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "./LoadingComponent";
 
 const mapStateToProps = (state) => {
   return {
@@ -23,6 +24,16 @@ class Directory extends Component {
       return <Tile title={item.name} caption={item.description} featured onPress={() => navigate("CampsiteInfo", { campsiteId: item.id })} imageSrc={{ uri: baseUrl + item.image }} />;
     };
 
+    if (this.props.campsites.isLoading) {
+      return <Loading />;
+    }
+    if (this.props.campsites.errMess) {
+      return (
+        <View>
+          <Text>{this.props.campsites.errMess}</Text>
+        </View>
+      );
+    }
     return <FlatList data={this.props.campsites.campsites} renderItem={renderDirectoryItem} keyExtractor={(item) => item.id.toString()} />;
   }
 }

7592f63b2fb1d772759728ebcd0fee405d429eeb
Robert Turner <robturneris@gmail.com>
2021-12-29 13:23:24 -0500

    Added post comment action creator

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -157,3 +157,17 @@ export const addFavorite = (campsiteId) => ({
   type: ActionTypes.ADD_FAVORITE,
   payload: campsiteId,
 });
+
+export const postComment = (campsiteId, rating, author, text) => (dispatch) => {
+  const newComment = {
+    campsiteId,
+    rating,
+    author,
+    text,
+  };
+  const d = new Date();
+  newComment.date = d.toISOString();
+  setTimeout(() => {
+    dispatch(addComment(newComment));
+  }, 2000);
+};

6c827110027b496e314a98f66a947199f570be54
Robert Turner <robturneris@gmail.com>
2021-12-29 13:42:21 -0500

    Added add comment action type to comments reducer

--- redux/comments.js
+++ redux/comments.js
@@ -5,6 +5,11 @@ export const comments = (state = { errMess: null, comments: [] }, action) => {
     case ActionTypes.ADD_COMMENTS:
       return { ...state, errMess: null, comments: action.payload };
 
+    case ActionTypes.ADD_COMMENT:
+      const newComment = action.payload;
+      newComment.id = state.comments.length;
+      return { ...state, errMess: null, comments: state.comments.concat(newComment) };
+
     case ActionTypes.COMMENTS_FAILED:
       return { ...state, errMess: action.payload };
 

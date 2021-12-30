2f8ad54c5d1a5e7b8293deed83bae11d21ef4508
Robert Turner <robturneris@gmail.com>
2021-12-30 17:02:56 -0500

    Added Favorites navigation to Main component

--- components/MainComponent.jsx
+++ components/MainComponent.jsx
@@ -14,6 +14,7 @@ import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import Constants from "expo-constants";
 import Reservation from "./ReservationComponent";
+import Favorites from "./FavoritesComponent";
 
 const mapDispatchToProps = {
   fetchCampsites,
@@ -21,6 +22,24 @@ const mapDispatchToProps = {
   fetchPromotions,
   fetchPartners,
 };
+
+const FavoritesNavigator = createStackNavigator(
+  {
+    Favorites: { screen: Favorites },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+      headerLeft: <Icon name="heart" type="font-awesome" iconStyle={styles.stackIcon} onPress={() => navigation.toggleDrawer()} />,
+    }),
+  }
+);
 
 const DirectoryNavigator = createStackNavigator(
   {
@@ -167,6 +186,13 @@ const MainNavigator = createDrawerNavigator(
       navigationOptions: {
         drawerLabel: "Contact Us",
         drawerIcon: ({ tintColor }) => <Icon name="address-card" type="font-awesome" size={24} color={tintColor} />,
+      },
+    },
+    Favorites: {
+      screen: FavoritesNavigator,
+      navigationOptions: {
+        drawerLabel: "My Favorites",
+        drawerIcon: ({ tintColor }) => <Icon name="heart" type="font-awesome" size={24} color={tintColor} />,
       },
     },
   },
